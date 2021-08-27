e1f3ad41dbf8e0a81e219668db9166c119e92996
Daniel Rodrigues <danielrdesousa@gmail.com>
2021-08-27 02:44:45 -0300

    create API

---
+++ routes/calendar.js
@@ -0,0 +1,94 @@
+const ics = require('ics')
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const express = require('express');
+const router = express.Router();
+
+const options = {
+    uri: 'https://www.ufc.br/calendario-universitario/2021',
+    transform: function (body) {
+        return cheerio.load(body)
+    }
+}
+
+router.get('/', async function(request, response, next) {
+  const type = request.query.type;
+  const events = await resquestExternal();
+
+  if(type && type == 'json') {
+    response.json({ 
+      created_at: new Date(),
+      targetURL: options.uri,
+      events
+    });
+  } else {
+    ics.createEvents(events, (error, generetedIcs) => {
+      if (error) console.log(error);
+
+      response.send(generetedIcs)
+    });
+  }
+});
+
+router.get('/download', async function(request, response, next) {
+  const events = await resquestExternal();
+
+  ics.createEvents(events, (error, generetedIcs) => {
+    if (error) console.log(error);
+
+    response.status(201)
+      .attachment(`calendar.ics`)
+      .send(generetedIcs)
+  });
+});
+
+
+async function resquestExternal() {
+  return rp(options).then(($) => {
+    const events = [];
+    const titles = [];
+
+    $('.c-calendarios').each((i, elem) => {
+      $(elem).find('h3').map((i, title) => {
+          titles.push(`${$(title).text().split(' ')[0]}/${$(title).text().split(' ')[2]}`)
+      });
+
+      $(elem).find('.category').map((i, table) => {
+          const monthYear  = titles[i];
+          const monthNames = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ];
+
+          let description = null;
+          $(table).find('td').map((i, item) => {
+              const td = $(item).text();
+              if(i % 2 == 0) {
+                  dateMonth = String(monthNames.indexOf(monthYear.split('/')[0])+1).padStart(2, '0');
+                  dateYear = monthYear.split('/')[1]
+                  dateMonthYear = `${dateMonth}/${dateYear}`;
+
+                  if(td.match(/ a | e /)){
+                      dateDayEnd = (td.split(' ')[2]).replace(/\D/g, "");
+                      dateDayStart = (td.split(' ')[0]).replace(/\D/g, "");
+                  } else {
+                      dateDayStart = td.replace(/\D/g, "");
+                      dateDayEnd = dateDayStart;
+                  };
+              } else {
+                  description = td;
+                  events.push({
+                      start: [parseInt(dateYear),parseInt(dateMonth),parseInt(dateDayStart)],
+                      end: [parseInt(dateYear),parseInt(dateMonth),parseInt(dateDayEnd)],
+                      title: description
+                  })    
+              }
+          });
+      });      
+    });
+
+    return events;
+  });
+}
+
+
+module.exports = router;
